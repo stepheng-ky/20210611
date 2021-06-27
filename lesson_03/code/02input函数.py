11d7485b6a60c4e89b7e0ed2eefc88838520013e
Stepheng <1142262478@qq.com>
2021-06-27 17:13:12 +0800

    input

---
+++ lesson_03/code/02input函数.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2021/6/27 16:56
+# @Author  : Stepheng
+# @Email   : 1142262478@qq.com
+# @File    : 02input函数.py
+# 功能描述  ：
+# 获取用户输入 input() 将用户输入的内容返回字符串
+#
+input_value = input('随便输入就是玩：')
+print('你输入的是：',input_value)
