63b5eda794d0bb718bed449cf8bb6690c54fb966
Stepheng <1142262478@qq.com>
2021-06-27 16:20:16 +0800

    第一次提交测试

---
+++ lesson_03/code/01条件判断语句.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2021/6/27 16:07
+# @Author  : Stepheng
+# @Email   : 1142262478@qq.com
+# @File    : 01条件判断语句.py
+# 功能描述  ：
+# 条件判断 / if语句
+# 语法： if ...:... else ...
