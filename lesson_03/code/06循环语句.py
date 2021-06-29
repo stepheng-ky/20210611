424cae019bd53f5e5c276cea423e24008f193774
Stepheng <1142262478@qq.com>
2021-06-29 22:16:05 +0800

    while2

--- lesson_03/code/06循环语句.py
+++ lesson_03/code/06循环语句.py
@@ -34,5 +34,39 @@ print(f'100以内7的倍数之和{sum}')
 print(f'100以内7的倍数有{count}个')
 
 
+# 水仙花
+# 一个三位以上数字，每个位上的数字的n次幂之和等于自己
+# （1**3 + 5**3 + 3**3 = 153）
+# 打印1000以内的水仙花数字
+i = 100
+n = 1000
+print(f'{i}到{n}之间的水仙花数字如下：')
+while i < n :
+    tihrd_num = i // 100
+    second_num = (i % 100) // 10
+    first_num = (i % 100) % 10
+    if tihrd_num ** 3 + second_num ** 3 + first_num ** 3 == i :
+        print(i)
+    i += 1
 
 
+# 输入一个数字 判断是否是质数
+num = int(input('来个数字给你判断是否是质数：'))
+i = 2
+# 是否是质数
+flag = True
+if num == 1:
+    print(f'{num}不是质数')
+elif num == 2:
+    print(f'{num}是质数')
+else:
+    while i <= num // 2 :
+        if num % i == 0:
+            flag = False
+            break
+        i += 1
+    if flag :
+        print(f'{num}是质数')
+    else:
+        print(f'{num}不是质数')
+    print(f'循环了{i}次')
