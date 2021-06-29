f7e67576c9edf21930c989e77f565dd52c02c5c4
Stepheng <1142262478@qq.com>
2021-06-29 22:57:02 +0800

    循环 break continue

---
+++ lesson_03/code/13break和continue.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2021/6/29 22:50
+# @Author  : Stepheng
+# @Email   : 1142262478@qq.com
+# @File    : 13break和continue.py
+# 功能描述  ：
+# break 立即退出循环
+# continue
+
+print('########## 测试break ##########')
+i = 1
+n = 10
+while i <= n :
+    i += 1
+    if i == 5 :
+        print('跳出循环')
+        break
+    print(i)
+else :
+    print('循环结束')
+
+print('########## 测试continue ##########')
+i = 1
+n = 10
+while i <= n :
+    i += 1
+    if i == 5 :
+        print('continue')
+        continue
+    print(i)
+else:
+    print('循环结束')
